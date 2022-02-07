c2955b50dc576c70a5d4673915e9ddd17fe7f135
kumapagu <kmno_555@yahoo.co.jp>
2022-02-07 22:18:33 +0900

    ファイルの変更

--- map_app/views.py
+++ map_app/views.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from basicauth.decorators import basic_auth_required
 
-# @method_decorator(basic_auth_required, name='dispatch')
+@method_decorator(basic_auth_required, name='dispatch')
 class IndexView(TemplateView):
     template_name = 'map_app/index.html'

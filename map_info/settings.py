20203a827221755cc55959e04a8c5efc75fcd032
kumapagu <kmno_555@yahoo.co.jp>
2022-02-08 22:10:53 +0900

    djangoとmysqlの接続

--- map_info/settings.py
+++ map_info/settings.py
@@ -91,8 +91,11 @@ WSGI_APPLICATION = 'map_info.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mapInfo_v_2',
+        'USER': 'root',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
